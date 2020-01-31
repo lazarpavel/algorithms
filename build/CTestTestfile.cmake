b8cfb3e43cde79e742ea2b81d07721d5011ae95b
Pavel Lazar <pavel.lazar@protonmail.com>
2020-01-31 18:18:02 -0500

    cleanup

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /development/workspaces/LZR.Algorithms
-# Build directory: /development/workspaces/LZR.Algorithms/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
