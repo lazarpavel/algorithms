88e19b7f20e5370d264be48750c7f0fcc3df502c
Pavel Lazar <pavel.lazar@protonmail.com>
2020-01-31 18:15:18 -0500

    added CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /development/workspaces/LZR.Algorithms
+# Build directory: /development/workspaces/LZR.Algorithms/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
